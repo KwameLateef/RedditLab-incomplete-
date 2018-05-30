cf82ec8d41f715b0a41645181711f3fe769ad364
Kwame Hampton <khamp1st@gmail.com>
2018-05-30 16:11:18 -0400

    RedditIncomplete

---
+++ js/components/service.js
@@ -0,0 +1,6 @@
+"use strict";
+
+
+angular
+.module("apiApp")
+.factory("service", ["$http", service]);
---
+++ js/components/template-component.js
@@ -0,0 +1,6 @@
+"use strict";
+
+
+angular 
+  .module("apiApp")
+  .component("templateComponent", templateComponent);
